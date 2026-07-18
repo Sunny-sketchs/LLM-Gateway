8e5636e51f0c55eff2e2de2ff6bd6a12441a92a4
Sunny-sketchs <sunnysb2004@gmail.com>
2026-07-18 17:38:16 +0530

    Middleware addad

--- src/app/main.py
+++ src/app/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from src.app.config import settings
 from src.app.routers.proxy import router
 from src.app.redis_setup.redis_client import redis_client
 
 
 app = FastAPI(title=settings.app_name, version=settings.app_version)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
 
 
